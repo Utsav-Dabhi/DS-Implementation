8379a9d71c3d8ff463e3aed659d2eb542474df2f
utsavd03 <85575604+utsavd03@users.noreply.github.com>
2023-01-10 10:59:52 +0530

    Circular Linked List all delete operations completed

--- CircularLinkedList.cpp
+++ CircularLinkedList.cpp
@@ -21,7 +21,8 @@ void append(int ele) {
 
         tail = new_node;
     } else {
-        new_node->next = tail;
+        new_node->next = tail->next;
+        tail->next = new_node;
         tail = new_node;
     }
 
@@ -63,16 +64,90 @@ void insert(int ele, int pos) {
     cllLength++;
 }
 
-void displayCLL() {
-    Node* ptr = head;
-
+void truncate() {
     if (head == NULL) {
-        cout << "\nNo elements to print" << endl;
+        cout << "\nNo elements to delete!" << endl;
         return;
     }
 
+    if (head == tail) {
+        head = tail = NULL;
+
+        cllLength--;
+
+        return;
+    } else {
+        int localLength = cllLength - 2;
+
+        Node* ptr = head;
+        while (localLength--) {
+            ptr = ptr->next;
+        }
+
+        ptr->next = head;
+        tail = ptr;
+
+        cllLength--;
+    }
+}
+
+void pop() {
+    if (head == NULL) {
+        cout << "\nNo elements to delete!" << endl;
+        return;
+    }
+
+    if (head == tail) {
+        head = tail = NULL;
+
+        cllLength--;
+
+        return;
+    } else {
+        Node* ptr = head->next;
+        head = ptr;
+
+        tail->next = ptr;
+
+        cllLength--;
+    }
+}
+
+void remove(int pos) {
+    if (head == NULL) {
+        cout << "\nNo elements to delete!" << endl;
+        return;
+    }
+
+    if (head == tail) {
+        head = tail = NULL;
+
+        cllLength--;
+
+        return;
+    } else {
+        pos -= 2;
+
+        Node* ptr = head;
+        while (pos--) {
+            ptr = ptr->next;
+        }
+
+        ptr->next = ptr->next->next;
+
+        cllLength--;
+    }
+}
+
+void displayCLL() {
+    if (head == NULL) {
+        cout << "\nNo elements to print!" << endl;
+        return;
+    }
+
+    Node* ptr = head;
     cout << "\nThe elements of Circular Linked List are: HEAD -> ";
-    while (ptr->next != head) {
+    while (ptr != tail) {
         cout << ptr->data << " -> ";
         ptr = ptr->next;
     }
@@ -83,8 +158,8 @@ void displayCLL() {
  
 int main() {
     int i = 0;
-    while (i != 5) {
-        cout << "\nWhat operation do you want to perform?\n1. Create a circular linked list\n2. Insert element (at end)\n3. Insert element (in beginning)\n4. Insert element (in between)\n5. Exit\nChoice: ";
+    while (i != 8) {
+        cout << "\nWhat operation do you want to perform?\n1. Create a circular linked list\n2. Insert element (at end)\n3. Insert element (in beginning)\n4. Insert element (in between)\n5. Delete element (from end)\n6. Delete element (from beginning)\n7. Delete element (from between)\n8. Exit\nChoice: ";
         cin >> i;
 
         switch(i) {
@@ -101,6 +176,7 @@ int main() {
                 }
 
                 displayCLL();
+
                 continue;
 
             case 2:
@@ -109,6 +185,7 @@ int main() {
 
                 append(num);
                 displayCLL();
+
                 continue;
             
             case 3:
@@ -143,6 +220,37 @@ int main() {
                 continue;
 
             case 5:
+                truncate();
+                displayCLL();
+                continue;
+
+            case 6:
+                pop();
+                displayCLL();
+                continue;
+
+            case 7:
+                cout << "\nEnter the element index's to be deleted: ";
+                cin >> pos;
+
+                while (pos > cllLength) {
+                    cout << "\nPosition greater than size of Linked List! Cannot enter element";
+                    cout << "\nRe-enter position: ";
+                    cin >> pos;
+                }
+
+                while (pos <= 0) {
+                    cout << "\nPosition cannot be less than 1";
+                    cout << "\nRe-enter position: ";
+                    cin >> pos;
+                }
+
+                remove(pos);
+                displayCLL();
+
+                continue;
+
+            case 8:
                 cout << "\nExited successfully";
                 break;
         }
