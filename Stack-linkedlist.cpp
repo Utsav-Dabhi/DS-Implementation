170e6e63e3c37ade987dae71e4e76b244d54d90a
utsavd03 <85575604+utsavd03@users.noreply.github.com>
2023-01-13 16:50:32 +0530

    Implemented Stack(linked list)

---
+++ Stack-linkedlist.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+struct Node* top = NULL;
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void push(int ele) {
+    Node* new_node = new Node;
+    new_node->data = ele;
+
+    if (top == NULL) {
+        new_node->next = NULL;
+    } else {
+        new_node->next = top;
+    }
+
+    top = new_node;
+}
+
+void displayStack() {
+    if (top == NULL) {
+        cout << "\nNo elements in stack to print!" << endl;
+    } else {
+        Node* ptr = top;
+
+        cout << "\nThe elements of stack are: top -> ";
+        while (ptr != NULL) {
+            cout << ptr->data << " ";
+            ptr = ptr->next;
+        }
+        cout << endl;
+    }
+}
+ 
+int main() {
+    int i = 0;
+    while (i != 6) {
+        cout << "\nWhat operation do you want to perform?\n1. Create a stack\n2. Push\n3. Pop\n4. Peek\n5. Is Empty?\n6. Exit\nChoice: ";
+        cin >> i;
+
+        switch(i) {
+            case 1:
+                int n;
+                cout << "\nHow many elements do you want to insert? ";
+                cin >> n;
+
+                int num;
+                for (int i = 0; i < n; i++) {
+                    cout << "Enter element " << i << ": ";
+                    cin >> num;
+                    push(num);
+                }
+
+                displayStack();
+
+                break;
+                
+            case 6:
+                cout << "\nExited successfully";
+                break;
+
+            default:
+                cout << "\nWrong Choice!" << endl;
+                break;
+        }
+    }
+    
+    return 0;
+}
