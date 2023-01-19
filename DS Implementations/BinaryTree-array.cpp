bea2e031766c8c0d2fd363cca497c0a894fc73f7
utsavd03 <85575604+utsavd03@users.noreply.github.com>
2023-01-19 09:29:20 +0530

    Binary Tree(array) delete operation completed

--- DS Implementations/BinaryTree-array.cpp
+++ DS Implementations/BinaryTree-array.cpp
@@ -13,33 +13,53 @@ void createBT(BinTree* bt, int level) {
     bt->level = level;
     bt->size = pow(2, level) - 1;
     bt->arr = new int[bt->size];
-    // bt->arr[0] = -2147483648;
 
     for (int i = 0; i < bt->size; i++) {
         bt->arr[i] = -2147483648;
     }
 }
 
-void leftChild(BinTree* bt, int ele, int pIndex) {
+void insertLeftChild(BinTree* bt, int ele, int pIndex) {
     if (bt->arr[pIndex] == -2147483648) {
-        cout << "\nParent not found, cannot set it's child" << endl;
+        cout << "\nParent not found, cannot set it's child!" << endl;
     } else {
         bt->arr[(pIndex * 2) + 1] = ele;
     }
 }
 
-void rightChild(BinTree* bt, int ele, int pIndex) {
+void insertRightChild(BinTree* bt, int ele, int pIndex) {
     if (bt->arr[pIndex] == -2147483648) {
-        cout << "\nParent not found, cannot set it's child" << endl;
+        cout << "\nParent not found, cannot set it's child!" << endl;
     } else {
         bt->arr[(pIndex * 2) + 2] = ele;
+    }
+}
+
+void deleteLeftChild(BinTree* bt, int pIndex) {
+    if (bt->arr[pIndex] == -2147483648) {
+        cout << "\nParent not found, cannot delete it's child!" << endl;
+    } else {
+        bt->arr[(pIndex * 2) + 1] = -2147483648;
+    }
+}
+
+void deleteRightChild(BinTree* bt, int pIndex) {
+    if (bt->arr[pIndex] == -2147483648) {
+        cout << "\nParent not found, cannot delete it's child!" << endl;
+    } else {
+        bt->arr[(pIndex * 2) + 2] = -2147483648;
     }
 }
 
 void printBT(BinTree* bt) {
     cout << "\nThe elements of binary tree are: ";
     for (int i = 0; i < bt->size; i++) {
-        cout << bt->arr[i] << " ";
+        cout << bt->arr[i];
+        if (i == 0) {
+            cout << "(-1) ";
+        } else {
+            cout << "(" << (i - 1) / 2 << ") ";
+        }
     }
     cout << endl;
 }
@@ -48,8 +68,8 @@ int main() {
     BinTree byT;
 
     int i = 0;
-    while (i != 4) {
-        cout << "\nWhat operation do you want to perform?\n1. Create a binary tree\n2. Insert left child\n3. Insert right child\n4. Exit\nChoice: ";
+    while (i != 6) {
+        cout << "\nWhat operation do you want to perform?\n1. Create a binary tree\n2. Insert left child\n3. Insert right child\n4. Delete left child\n5. Delete right child\n6. Exit\nChoice: ";
         cin >> i;
 
         switch(i) {
@@ -70,26 +90,68 @@ int main() {
                 break;
             
             case 2:
+                cout << "\nParent's index: \n";
+                cout << "    A(0)\n";
+                cout << "   / \\\n";
+                cout << "  /   \\\n";
+                cout << "B(1)   C(2)";
+
                 int ele, pIndex;
                 cout << "\nEnter the element and parent's index: ";
                 cin >> ele >> pIndex;
 
-                leftChild(&byT, ele, pIndex);
+                insertLeftChild(&byT, ele, pIndex);
                 printBT(&byT);
 
                 break;
             
             case 3:
+                cout << "\nParent's index: \n";
+                cout << "    A(0)\n";
+                cout << "   / \\\n";
+                cout << "  /   \\\n";
+                cout << "B(1)   C(2)";
+
                 cout << "\nEnter the element and parent's index: ";
                 cin >> ele >> pIndex;
 
-                rightChild(&byT, ele, pIndex);
+                insertRightChild(&byT, ele, pIndex);
                 printBT(&byT);
 
                 break;
 
             case 4:
-                cout << "Exited successfully";
+                cout << "\nParent's index: \n";
+                cout << "    A(0)\n";
+                cout << "   / \\\n";
+                cout << "  /   \\\n";
+                cout << "B(1)   C(2)";
+
+                cout << "\nEnter parent's index: ";
+                cin >> pIndex;
+
+                deleteLeftChild(&byT, pIndex);
+                printBT(&byT);
+
+                break;
+            
+            case 5:
+                cout << "\nParent's index: \n";
+                cout << "    A(0)\n";
+                cout << "   / \\\n";
+                cout << "  /   \\\n";
+                cout << "B(1)   C(2)";
+
+                cout << "\nEnter parent's index: ";
+                cin >> pIndex;
+
+                deleteRightChild(&byT, pIndex);
+                printBT(&byT);
+
+                break;
+
+            case 6:
+                cout << "\nExited successfully";
         }
     }
     
