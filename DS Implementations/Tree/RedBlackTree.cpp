a36b744d3fde01b0b5ed80137f05aae97551aa38
utsavd03 <85575604+utsavd03@users.noreply.github.com>
2023-02-11 22:37:04 +0530

    Red Black Tree delete operation completed

--- DS Implementations/Tree/RedBlackTree.cpp
+++ DS Implementations/Tree/RedBlackTree.cpp
@@ -7,21 +7,25 @@ const bool BLACK = true;
 struct RBTNode* root = NULL;
 
 // color = false(red) & = true(black)
-struct RBTNode {
-    int key;
-    RBTNode *left, *right, *parent;
-    bool color;
+class RBTNode {
+    public:
+        int key;
+        RBTNode *left, *right, *parent;
+        bool color;
+
+        RBTNode(int key) {
+            this->key = key;
+            this->left = this->right = this->parent = NULL;
+            this->color = RED;
+        }
 };
 
-RBTNode* createNode(int data) {
-    RBTNode* new_node = new RBTNode;
-
-    new_node->key = data;
-    new_node->left = new_node->right = new_node->parent = NULL;
-    new_node->color = RED;
-
-    return new_node;
-}
+class NilNode: public RBTNode {
+    public:
+        NilNode(): RBTNode(-999) {
+            this->color = BLACK;
+        }
+};
 
 void replaceParentsChild(RBTNode* parent, RBTNode* oldChild, RBTNode* newChild) {
     if (parent == NULL) {
@@ -77,7 +81,7 @@ RBTNode* getUncle(RBTNode* parent) {
     }
 }
 
-void fixViolations(RBTNode* node) {
+void fixViolationsAfterInsert(RBTNode* node) {
     RBTNode* parent = node->parent;
 
     // Case 1: Parent is null, we've reached the root, the end of the recursion
@@ -106,7 +110,7 @@ void fixViolations(RBTNode* node) {
 
         // Call recursively for grandparent, which is now red.
         // It might be root or have a red parent, in which case we need to fix more...
-        fixViolations(grandparent);
+        fixViolationsAfterInsert(grandparent);
     }
 
     // Parent is left child of grandparent
@@ -165,7 +169,7 @@ void insertKey(int key) {
     }
 
     // insert new node
-    RBTNode* newNode = createNode(key);
+    RBTNode* newNode = new RBTNode(key);
 
     if (parent == NULL) {
         root = newNode;
@@ -176,7 +180,188 @@ void insertKey(int key) {
     }
     newNode->parent = parent;
 
-    fixViolations(newNode);
+    fixViolationsAfterInsert(newNode);
+}
+
+RBTNode* deleteNodeWithZeroOrOneChild(RBTNode* node) {
+    // if node has only a left child -> replace with left child
+    if (node->left != NULL) {
+        replaceParentsChild(node->parent, node, node->left);
+        return node->left; // moved-up node
+    }
+    // if node has only a right child -> replace with right child
+    else if (node->right != NULL) {
+        replaceParentsChild(node->parent, node, node->right);
+        return node->right; // moved-up node
+    }
+
+    // if node has no children -->
+    // * node is red -> just remove it
+    // * node is black -> replace it by a temporary NIL node (needed to fix the R-B rules)
+    else {
+        RBTNode* newChild = node->color == BLACK ? new NilNode() : NULL;
+        replaceParentsChild(node->parent, node, newChild);
+        return newChild;
+    }
+}
+
+RBTNode* findMinimum(RBTNode* node) {
+    RBTNode* current = node;
+
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+
+    return current;
+}
+
+RBTNode* getSibling(RBTNode* node) {
+    RBTNode* parent = node->parent;
+
+    if (parent->left == node) {
+        return parent->right;
+    } else if (parent->right == node) {
+        return parent->left;
+    } else {
+        return NULL;
+    }
+}
+
+void handleRedSibling(RBTNode* node, RBTNode* sibling) {
+    // recolor ..
+    sibling->color = BLACK;
+    node->parent->color = RED;
+
+    // .. and rotate
+    if (node->parent->left == node) {
+        leftRotate(node->parent);
+    } else {
+        rightRotate(node->parent);
+    }
+}
+
+bool isBlack(RBTNode* node) {
+    return node == NULL || node->color == BLACK;
+}
+
+void handleBlackSiblingWithAtLeastOneRedChild(RBTNode* node, RBTNode* sibling) {
+    bool nodeIsLeftChild = node == node->parent->left;
+
+    // Case 5: Black sibling with at least one red child + "outer nephew" is black
+    // --> Recolor sibling and its child, and rotate around sibling
+    if (nodeIsLeftChild && isBlack(sibling->right)) {
+        sibling->left->color = BLACK;
+        sibling->color = RED;
+        rightRotate(sibling);
+        sibling = node->parent->right;
+    } else if (!nodeIsLeftChild && isBlack(sibling->left)) {
+        sibling->right->color = BLACK;
+        sibling->color = RED;
+        leftRotate(sibling);
+        sibling = node->parent->left;
+    }
+
+    // Fall-through to case 6...
+
+    // Case 6: Black sibling with at least one red child + "outer nephew" is red
+    // --> Recolor sibling + parent + sibling's child, and rotate around parent
+    sibling->color = node->parent->color;
+    node->parent->color = BLACK;
+    if (nodeIsLeftChild) {
+        sibling->right->color = BLACK;
+        leftRotate(node->parent);
+    } else {
+        sibling->left->color = BLACK;
+        rightRotate(node->parent);
+    }
+}
+
+void fixViolationsAfterDelete(RBTNode* node) {
+    // Case 1: Examined node is root, end of recursion
+    if (node == root) {
+        node->color = BLACK; // enforcing black roots
+        return;
+    }
+
+    RBTNode* sibling = getSibling(node);
+
+    // Case 2: Red sibling
+    if (sibling->color == RED) {
+        handleRedSibling(node, sibling);
+        sibling = getSibling(node); // Get new sibling for fall-through to cases 3-6
+    }
+
+    // Cases 3+4: Black sibling with two black children
+    if (isBlack(sibling->left) && isBlack(sibling->right)) {
+        sibling->color = RED;
+
+        // Case 3: Black sibling with two black children + red parent
+        if (node->parent->color == RED) {
+            node->parent->color = BLACK;
+        }
+
+        // Case 4: Black sibling with two black children + black parent
+        else {
+            fixViolationsAfterDelete(node->parent);
+        }
+    }
+
+    // Case 5+6: Black sibling with at least one red child
+    else {
+        handleBlackSiblingWithAtLeastOneRedChild(node, sibling);
+    }
+}
+
+void deleteKey(int key) {
+    RBTNode* node = root;
+
+    // Find the node to be deleted
+    while (node != NULL && node->key != key) {
+        // Traverse the tree to the left or right depending on the key
+        if (key < node->key) {
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+    }
+
+    // node not found?
+    if (node == NULL) {
+        return;
+    }
+
+    // In this variable, we'll store the node at which we're going to start to fix the R-B 
+    // properties after deleting a node.
+    RBTNode* movedUpNode;
+    bool deletedNodeColor;
+
+    // if node has zero or one child
+    if (node->left == NULL || node->right == NULL) {
+        movedUpNode = deleteNodeWithZeroOrOneChild(node);
+        deletedNodeColor = node->color;
+    }
+
+    // if node has two children
+    else {
+        // Find minimum node of right subtree ("inorder successor" of current node)
+        RBTNode* inorderSuccessor = findMinimum(node->right);
+
+        // Copy inorder successor's data to current node (keep its color!)
+        node->key = inorderSuccessor->key;
+
+        // Delete inorder successor just as we would delete a node with 0 or 1 child
+        movedUpNode = deleteNodeWithZeroOrOneChild(inorderSuccessor);
+        deletedNodeColor = inorderSuccessor->color;
+    }
+
+    if (deletedNodeColor == BLACK) {
+        fixViolationsAfterDelete(movedUpNode);
+
+        // Remove the temporary NIL node
+        if (typeid(*movedUpNode) == typeid(NilNode)) {
+            replaceParentsChild(movedUpNode->parent, movedUpNode, NULL);
+        }
+    }
 }
 
 // functions related to printing
@@ -222,6 +407,15 @@ int main() {
                 cin >> ele;
                 
                 insertKey(ele);
+                printTree();
+
+                break;
+
+            case 3:
+                cout << "\nEnter element to be deleted: ";
+                cin >> ele;
+                
+                deleteKey(ele);
                 printTree();
 
                 break;
